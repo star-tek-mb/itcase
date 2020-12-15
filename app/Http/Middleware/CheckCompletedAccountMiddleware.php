f252e2546702dc4dd995da5e825571d56bc43961
jtscorp@mail.ru <j19962323>
2020-12-15 17:31:12 +0500

    settings

--- app/Http/Middleware/CheckCompletedAccountMiddleware.php
+++ app/Http/Middleware/CheckCompletedAccountMiddleware.php
@@ -16,28 +16,16 @@ class CheckCompletedAccountMiddleware
     public function handle($request, Closure $next)
     {
         $user = auth()->user();
-        if (substr($request->path(), 0, 3) === "api") {
-            if ($user) {
-                if ($user->checkCompletedAccount()) {
-                    if ($user->hasRole('contractor') && $user->categories()->count() == 0 && $request->path() !== 'api/account/professional') {
-                        return response()->json(['message' => ['Выберите услуги которые вы предоставляете']]);
-                    }
-                    return $next($request);
-                } else {
-                    return response()->json(['message' => ['Для начала заполните данные']]);
-                }
+        if ($user) {
+            if ($user->checkCompletedAccount()) {
+                if ($user->hasRole('contractor') && $user->categories()->count() == 0 && $request->path() !== 'account/professional')
+                    return redirect('/account/professional')->with('account.warning', 'Выберите услуги, которые вы предоставляете');
+                return $next($request);
             }
-            return response()->json(['message' => ['Войдите в аккаунт']]);
-        } else {
-            if ($user) {
-                if ($user->checkCompletedAccount()) {
-                    if ($user->hasRole('contractor') && $user->categories()->count() == 0 && $request->path() !== 'account/professional')
-                        return redirect('/account/professional')->with('account.warning', 'Выберите услуги, которые вы предоставляете');
-                    return $next($request);
-                }
-                else return redirect('/account/create')->with('warning', 'Для начала заполните данные');
-            }
-            return redirect()->route('login');
+            else
+                return redirect('/account/create')->with('warning', 'Для начала заполните данные');
         }
+
+        return redirect()->route('login');
     }
 }
