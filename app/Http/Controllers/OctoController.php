44b73139add956a7aa0ec5e5de29b10847e91201
star-tek-mb <noob.tek.mb@gmail.com>
2021-04-09 21:27:37 +0500

    fix octo controller

--- app/Http/Controllers/OctoController.php
+++ app/Http/Controllers/OctoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Repositories\User;
 use Illuminate\Http\Request;
-use App\Services\Octo;
+use App\Services\OctoService;
 
 class OctoController extends Controller
 {
