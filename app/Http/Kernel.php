5ed594b27fcacf32676e235ee6123ae1fb357cab
star-tek-mb <noob.tek.mb@gmail.com>
2021-04-14 11:55:17 +0500

    sanctum switch

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'guardswitcher:api',
+            'guardswitcher:sanctum',
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             'bindings',
