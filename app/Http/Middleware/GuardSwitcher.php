721aec02b15c05895e50e8c710537d548e8d9f7d
star-tek-mb <noob.tek.mb@gmail.com>
2021-04-14 11:53:53 +0500

    guard switcher for api

---
+++ app/Http/Middleware/GuardSwitcher.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class GuardSwitcher
+{
+
+    public function handle($request, Closure $next, $defaultGuard = null) {
+        if (in_array($defaultGuard, array_keys(config("auth.guards")))) {
+           config(["auth.defaults.guard" => $defaultGuard]);
+        }
+        return $next($request);
+    }
+}
