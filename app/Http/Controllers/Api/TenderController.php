5d158f7b2348c7ee3a8887617dbbc64f8987a44a
Shokhzod <you@example.com>
2021-04-27 10:29:36 +0500

    delete

--- app/Http/Controllers/Api/TenderController.php
+++ app/Http/Controllers/Api/TenderController.php
@@ -342,6 +342,7 @@ class TenderController extends Controller
     {
         $tender =   $this->tenderRepository->get($id);
         $tender->delete_reason = $request->delete_reason;
+        $tender->opened = 0;
         $tender->save();
         Log::info($request->delete_reason);
         Log::info($request->isMethod('put'));
