086799cdf0409e020c30d666b95ac0fe6ada27a9
Shokhzod <you@example.com>
2021-05-03 11:45:50 +0500

    Fixed notification

--- app/Http/Controllers/Api/ContractorsController.php
+++ app/Http/Controllers/Api/ContractorsController.php
@@ -77,13 +77,13 @@ class ContractorsController extends Controller
         $contractors = PaginateCollection::paginateCollection($contractors, 5);
         foreach ($contractors as $contractor) {
             $comments = $this->users->getComments($contractor->id);
-            $mean = (int) collect($comments)->avg('assessment');
+            $mean = (int)collect($comments)->avg('assessment');
             $contractor->comments = $comments;
             $contractor->mean = $mean;
         }
         return response()->json([
-            'contractors'=>$contractors,
-            'contractorsCount'=>$contractorsCount
+            'contractors' => $contractors,
+            'contractorsCount' => $contractorsCount
         ]);
     }
 
@@ -95,10 +95,10 @@ class ContractorsController extends Controller
         $contractors = PaginateCollection::paginateCollection($contractors, 5);
         foreach ($contractors as $contractor) {
             $comments = $this->users->getComments($contractor->id);
-            $mean = (int) collect($comments)->avg('assessment');
+            $mean = (int)collect($comments)->avg('assessment');
             $contractor->mean = $mean;
-            $contractor->categories = $contractor->categories->map(function ($categories) use($contractor){
-                if ($contractor->pivot == null){
+            $contractor->categories = $contractor->categories->map(function ($categories) use ($contractor) {
+                if ($contractor->pivot == null) {
                     $contractor->pivot = $categories->pivot;
                 }
                 return [
@@ -110,8 +110,8 @@ class ContractorsController extends Controller
 
         }
         return response()->json([
-            'contractors'=>$contractors,
-            'contractorsCount'=>$contractorsCount
+            'contractors' => $contractors,
+            'contractorsCount' => $contractorsCount
         ]);
     }
 
@@ -121,7 +121,7 @@ class ContractorsController extends Controller
      * @param string $params
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
      */
-    public function category(Request $request,int $category_id)
+    public function category(Request $request, int $category_id)
     {
         $category = $this->categories->get($category_id);
         if ($category) {
@@ -130,9 +130,9 @@ class ContractorsController extends Controller
             $contractors = PaginateCollection::paginateCollection($contractors, 5);
             foreach ($contractors as $contractor) {
                 $comments = $this->users->getComments($contractor->id);
-                $mean = (int) collect($comments)->avg('assessment');
+                $mean = (int)collect($comments)->avg('assessment');
                 $contractor->mean = $mean;
-                $contractor->categories = $contractor->categories->map(function ($categories){
+                $contractor->categories = $contractor->categories->map(function ($categories) {
                     return [
                         'ru_title' => $categories->ru_title,
                         'en_title' => $categories->en_title,
@@ -142,8 +142,8 @@ class ContractorsController extends Controller
             }
 
             return response()->json([
-                'contractors'=>$contractors,
-                'contractorsCount'=>$contractorsCount
+                'contractors' => $contractors,
+                'contractorsCount' => $contractorsCount
             ]);
         } else {
             return response()->json([
@@ -168,7 +168,7 @@ class ContractorsController extends Controller
             ], 404);
         }
         $contractor->portfolio = $this->users->getPortfolioBySlug($contractor->slug);
-        $contractor->comments = $this->users->getComments($contractor->id)->get()->map(function ($com){
+        $contractor->comments = $this->users->getComments($contractor->id)->get()->map(function ($com) {
             $author = $com->author;
             $com->who_set = $author->first_name . " " . $author->last_name;
             return $com;
@@ -176,7 +176,7 @@ class ContractorsController extends Controller
 
 
         return response()->json([
-            'contractor'=>$contractor,
+            'contractor' => $contractor,
         ]);
     }
 
@@ -185,16 +185,19 @@ class ContractorsController extends Controller
         $request = $this->tenders->addContractor($tenderId, $contractorId);
         try {
             $this->users->get($contractorId)->notify(new InviteRequest($request));
-        } catch (\Exception $e) {}
+        } catch (\Exception $e) {
+        }
         return response()->json([
-            'success'=>'Исполнитель добавлен в конкурс!',
+            'success' => 'Исполнитель добавлен в конкурс!',
         ]);
     }
-    public  function acceptInvitation(Request $request){
+
+    public function acceptInvitation(Request $request)
+    {
         $user = auth()->user();
         $tenderId = $request->tenderId;
-        $tenderRequest = $user->requests()->where('tender_id',$tenderId)->where('invited', 1)->first();
-        if ($tenderRequest){
+        $tenderRequest = $user->requests()->where('tender_id', $tenderId)->where('invited', 1)->first();
+        if ($tenderRequest) {
             $tender = $this->tenders->get($tenderId);
             $tender->contractor_id = $tenderRequest->user_id;
             $tender->opened = false;
@@ -203,44 +206,49 @@ class ContractorsController extends Controller
             $tenderRequest->user->save();
             $requests = $tenderRequest->tender->requests;
             try {
-            $tender->owner->notify(new RequestAction('accepted_by_contractor',$tenderRequest));
-
-                foreach ($requests as $otherRequest) {
-                    if ($otherRequest->user_id == $user->id) {
-                        continue;
-                    }
-                    $otherRequest->user->notify(new RequestAction('rejected', $otherRequest, $otherRequest->tender));
-                }
-
-                $adminUsers = $this->users->getAdmins();
-                Notification::send($adminUsers, new RequestAction('accepted_by_contractor', $request));
+                $tender->owner->notify(new RequestAction('accepted_by_contractor', $tenderRequest));
             } catch (\Swift_TransportException $e) {
 
             }
 
+
+            foreach ($requests as $otherRequest) {
+                if ($otherRequest->user_id == $user->id) {
+                    continue;
+                }
+                try {
+                    $otherRequest->user->notify(new RequestAction('rejected', $otherRequest, $otherRequest->tender));
+                } catch (\Swift_TransportException $e) {
+
+                }
+            }
+
+            $adminUsers = $this->users->getAdmins();
+            Notification::send($adminUsers, new RequestAction('accepted_by_contractor', $request));
+
             return response()->json([
                 'success' => 'Вы приняли пришлашение. Скоро свами свяжуться'
-            ],200);
+            ], 200);
         } else {
             return response()->json([
                 'success' => 'Невозможно назначить исполнителя на этот конкурс'
-            ],401);
+            ], 401);
         }
     }
 
-    public  function  rejectInvitation(Request $request){
+    public function rejectInvitation(Request $request)
+    {
         $user = auth()->user();
         $tenderId = $request->tenderId;
-        $requestGet = $user->requests()->where('tender_id',$tenderId)->where('invited', 1)->first();
+        $requestGet = $user->requests()->where('tender_id', $tenderId)->where('invited', 1)->first();
         try {
             $requestGet->tender->owner->notify(new RequestAction('rejected_by_contractor', $requestGet));
-        }
-        catch (\Swift_TransportException $e) {
+        } catch (\Swift_TransportException $e) {
 
         }
         $requestGet->delete();
         return response()->json([
             'success' => 'Вы успешно отклонили предложение'
-        ],200);
+        ], 200);
     }
 }
