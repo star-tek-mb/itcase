920193130548260d9b7dcbfc23ffaa65601a91ad
star-tek-mb <noob.tek.mb@gmail.com>
2021-05-23 18:57:43 +0500

    realize sms problems with various operators

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -7,6 +7,18 @@ use Str;
 
 class SmsService
 {
+
+    private function isProblemOperators($phone)
+    {
+        // MTS, Yota, Megafon
+        if (preg_match("/\\+7(901|902|904|908|910|911|912|913|914|915|916|917|918|919|950|978|980|981|982|983|984|985|986|987|988|989)/", $phone) ||
+            preg_match("/\\+7(902|904|908|920|921|922|923|924|925|926|927|928|929|930|931|932|933|934|936|937|938|939|950|951|999)/", $phone))
+        {
+            return true;
+        }
+        return false;
+    }
+
     public function send($phone, $message)
     {
         $client = new Client([
@@ -19,7 +31,7 @@ class SmsService
                     'recipient' => $phone,
                     'message-id' => Str::random(20),
                     'sms' => [
-                        'originator' => 'ITCASE',
+                        'originator' => $this->isProblemOperators($phone) ? 'DOSTAVKA' : 'ITCASE',
                         'content' => [
                             'text' => $message
                         ]
