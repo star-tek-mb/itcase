d767f0491250e21f40f27d781b4f8d8762ebf195
Shokhzod <you@example.com>
2021-06-01 11:34:29 +0500

    Sd

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,7 +62,7 @@ class User extends Authenticatable
 //    }
     public function routeNotificationForFcm()
     {
-        return $this->messageTokens;
+        return $this->messageTokens()->first();
     }
 
 
