9e3d9abca730be774fcd0a51ec2e2e58d672330e
star-tek-mb <noob.tek.mb@gmail.com>
2021-06-01 15:13:32 +0500

    fix route name

--- resources/views/site/pages/contractors/show.blade.php
+++ resources/views/site/pages/contractors/show.blade.php
@@ -216,7 +216,7 @@
 <div class="modal" id="modal">
   <h4>Выберите задание, в который вы хотите пригласить исполнителя</h4>
   <select id="add-form" style="margin-top: 20px;">
-      <option value="{{ route('site.tenders.create') }}">У вас нету опубликованных заданий</option>
+      <option value="{{ route('site.tenders.common.create') }}">У вас нету опубликованных заданий</option>
       @foreach(auth()->user()->ownedTenders as $tender)
           @continue(!$tender->opened || $tender->status == 'done')
           @if ($tender->hasRequestFrom($contractor->id))
