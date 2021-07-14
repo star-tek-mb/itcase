d426eb5a782a6e0ce800087b4f5690b12fb48f98
Shokhzod <you@example.com>
2021-07-14 16:38:52 +0500

    ASD'

--- app/Services/RobokassaService.php
+++ app/Services/RobokassaService.php
@@ -16,7 +16,7 @@ class RobokassaService
     {
 
         $this->userRepository = new UserRepository();
-        $this->OutSum = 2145;
+        $this->OutSum = 10;
     }
 
     public function collectData($user, $type_device = 0)
